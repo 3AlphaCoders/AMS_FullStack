655dc7df5f87fcfc9482bf403645ae22b8d1ccc7
mainlycricket <metusharjain@gmail.com>
2023-01-26 13:32:03 +0530

    Port Trial

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.use("/api/v1/notice", [authenticateUser, noticeRouter]);
 app.use(notFoundHandler);
 app.use(errorHandlerMiddleware);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 const start = async () => {
   try {
